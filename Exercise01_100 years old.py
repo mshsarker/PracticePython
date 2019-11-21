7439cbde613825df7a46ad5bf9b77708bd28b81f
Msh Sarker <52801003+mshsarker@users.noreply.github.com>
2019-11-20 18:42:45 -0700

    Update Exercise01_100 years old.py

--- Exercise01_100 years old.py
+++ Exercise01_100 years old.py
@@ -4,5 +4,6 @@ Age = int(input("What's your age?: "))
 Year = str(100 - Age + 2019)
 Year2 = str(50 - Age + 2019)
 
-print( Name + ", You will turn 100 at " + Year  )
 print(Name + ", You will turn 50 at " + Year2 )
+print( Name + ", You will turn 100 at " + Year  )
+
